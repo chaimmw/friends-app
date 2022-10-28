2b0e827f319d33d70e0adc9f316a168bff8fba65
chaim <chaim.weiss@decisionlink.com>
2022-10-28 01:02:30 -0400

    create grid list

--- src/app/friends/components/friend-list/friend-list.component.ts
+++ src/app/friends/components/friend-list/friend-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Friend, FriendViewItem } from 'src/app/models/friend.model';
+import { FriendService } from 'src/app/services/friend.service';
+import { myFakeFriends } from 'src/assets/friends-data';
 
 @Component({
   selector: 'app-friend-list',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendListComponent implements OnInit {
 
-  constructor() { }
+  friends = myFakeFriends;
+
+  friendWData: FriendViewItem[] | undefined;
+  constructor(private friendService: FriendService) { }
 
   ngOnInit(): void {
+    this.friendWData = this.friends.map(friend => this.addFriendViewData(friend));
   }
 
+
+  addFriendViewData(friend: Friend): FriendViewItem {
+    return {
+      ...friend,
+      friendsNames: ['Larry', 'Barry', 'Joe']
+    }
+  }
 }
