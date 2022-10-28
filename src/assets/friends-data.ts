7b108990ae14b764801e83f7ccc0cfb1435f432a
chaim <chaim.weiss@decisionlink.com>
2022-10-28 00:05:55 -0400

    add fake friends

---
+++ src/assets/friends-data.ts
@@ -0,0 +1,34 @@
+import { Friend } from "src/app/models/friend.model";
+
+export const myFakeFriends: Friend[] = [
+  {
+    name: 'George Washington',
+    age: 300,
+    weight: 250,
+    friends: []
+  },
+  {
+    name: 'Micheal Jordan',
+    age: 55,
+    weight: 100,
+    friends: []
+  },
+  {
+    name: 'Mickey Mouse',
+    age: 8,
+    weight: 26,
+    friends: []
+  },
+  {
+    name: 'Donald Duck',
+    age: 45,
+    weight: 500,
+    friends: []
+  },
+  {
+    name: 'Thomas Jefferson',
+    age: 300,
+    weight: 166,
+    friends: []
+  },
+];
