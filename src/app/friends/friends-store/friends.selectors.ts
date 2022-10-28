4ccef4a1708824bf7a88e44457c5f0258e4a089d
chaim <chaim.weiss@decisionlink.com>
2022-10-27 23:51:06 -0400

    add selectors

--- src/app/friends/friends-store/friends.selectors.ts
+++ src/app/friends/friends-store/friends.selectors.ts
@@ -4,3 +4,15 @@ import * as fromFriends from './friends.reducer';
 export const selectFriendsState = createFeatureSelector<fromFriends.State>(
   fromFriends.friendsFeatureKey
 );
+
+const { selectAll } = fromFriends.adapter.getSelectors();
+
+export const allFriends = selectAll;
+
+export const getFriends = (friendIds: number[]) =>
+  createSelector(selectFriendsState, (state) =>
+    friendIds.map((id) => state.entities[id])
+  );
+
+export const getFriend = (id: number) =>
+  createSelector(selectFriendsState, (state) => state.entities[id]);
