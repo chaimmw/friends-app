72a3bc239df2178b2d3f6eaaac03f0095e6d67f2
chaim <chaim.weiss@decisionlink.com>
2022-10-30 01:22:35 -0400

    styling for cards

--- src/app/friends/components/friend-form-container/friend-form-container.component.ts
+++ src/app/friends/components/friend-form-container/friend-form-container.component.ts
@@ -6,27 +6,32 @@ import { FriendService } from 'src/app/services/friend.service';
 @Component({
   selector: 'app-friend-form-container',
   templateUrl: './friend-form-container.component.html',
-  styleUrls: ['./friend-form-container.component.css']
+  styleUrls: ['./friend-form-container.component.css'],
 })
 export class FriendFormContainerComponent implements OnInit {
-
   friends$: Observable<Friend[]>;
   selectedFriend: any;
   selectedFriend$: Observable<Friend>;
-  constructor(private friendsService: FriendService) { }
+  constructor(private friendsService: FriendService) {}
 
   ngOnInit(): void {
     this.friends$ = this.friendsService.allFriends$.pipe(
       map((buddies) => buddies.map((bud) => bud as Friend))
     );
-    this.selectedFriend$ = this.friendsService.selectedFriend as Observable<Friend>;
+    this.selectedFriend$ = this.friendsService
+      .selectedFriend as Observable<Friend>;
   }
 
   formSubmit(data: any) {
-    this.selectedFriend$.pipe(
-      tap((frnd) => frnd ? this.friendsService.updateFriend(frnd.id as string, data) : this.friendsService.addFriend(data)),
-      take(1)
-    ).subscribe();
+    this.selectedFriend$
+      .pipe(
+        tap((frnd) =>
+          frnd
+            ? this.friendsService.updateFriend(frnd.id as string, data)
+            : this.friendsService.addFriend(data)
+        ),
+        take(1)
+      )
+      .subscribe();
   }
-
 }
